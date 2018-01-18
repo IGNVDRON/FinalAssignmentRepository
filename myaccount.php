9e9f69ecd746534830deb2a86e04b9e6e9959770
hisnik636 <33879890+hisnik636@users.noreply.github.com>
2018-01-19 01:41:47 +0200

    changes

--- myaccount.php
+++ myaccount.php
@@ -7,8 +7,13 @@
 	Signed in.
 	Hello, <?php echo $_SESSION['logged_user']->login; ?>
 	<hr>
-	<a href="shop/logout.php">Sign out</a>
+	<a href="logout.php">Sign out</a>
 	<?php else : ?>
+<<<<<<< HEAD
 	<a href="../FinalAssignmentRepository/login.php">Authorization</a><br>
 	<a href="/FinalAssignmentRepository/signup.php">Registration</a>
+=======
+	<a href="login.php">Authorization</a><br>
+	<a href="signup.php">Registration</a>
+>>>>>>> 96281c8333b3a0d4af8ee78ad0a9641a9ff8a61a
 	<?php endif; ?>
